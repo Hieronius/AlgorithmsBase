a13566fb9587e6ecb7d134add7e17a61c856c21c
Arsenty Halimovsky <ghost12@yandex.ru>
2023-06-05 17:05:34 +0300

    Create a new project

---
+++ AlgorithmPractice/AlgorithmPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlgorithmPractice
+//
+//  Created by Арсентий Халимовский on 05.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
