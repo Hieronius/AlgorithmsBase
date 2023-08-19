46bf6ba420c146ea7bbe9168ff05652e06e275fa
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-19 04:54:19 +0300

    Remove solution

--- AlgorithmPractice/AlgorithmPractice/Homework Tasks/While.swift
+++ AlgorithmPractice/AlgorithmPractice/Homework Tasks/While.swift
@@ -564,22 +564,33 @@ func removeSpaces(string: String) -> String {
 
 func countMinWord2(_ string: String) -> Int {
     var index = 0
-    var internalString = String()
-    var arrayOfCharsAmount = [Int]()
-    var stringArray = Array(string)
+    var count = 0
+    var internalArray = Array(string)
+    var arrayOfWordsCount = [Int]()
     
-    while index != stringArray.count {
-        if stringArray[index] == " " {
-            arrayOfCharsAmount.append(internalString.count)
-            internalString = String()
+    while index != internalArray.count{
+        if internalArray[index] == " " || index == internalArray.count {
+            arrayOfWordsCount.append(count)
+            count = 0
             index += 1
         } else {
-            internalString.append(stringArray[index])
+            count += 1
             index += 1
         }
     }
-    arrayOfCharsAmount.append(internalString.count)
-    return arrayOfCharsAmount.sorted(by: <)[0]
+     arrayOfWordsCount.append(count)
+    
+    var subIndex = 0
+    var firstCountNumber = arrayOfWordsCount[0]
+    while subIndex != arrayOfWordsCount.count {
+        if arrayOfWordsCount[subIndex] < firstCountNumber {
+            firstCountNumber = arrayOfWordsCount[subIndex]
+            subIndex += 1
+        } else {
+            subIndex += 1
+        }
+    }
+    return firstCountNumber
 }
 
 
