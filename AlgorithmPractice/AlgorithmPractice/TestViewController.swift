274cf5a3971ee19277c49e03b65232b2fec4ecc0
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-27 04:18:09 +0300

    add variation of solution to problem

--- AlgorithmPractice/AlgorithmPractice/TestViewController.swift
+++ AlgorithmPractice/AlgorithmPractice/TestViewController.swift
@@ -12,9 +12,44 @@ import Foundation
 
 class TestViewController: UIViewController {
     
+    // MARK: Task 26 ❌
+
+    /*
+     (string.enumerated, удаление элементов)
+     
+     26. Удалить первый и последний элемент строки
+     
+     "place" -> "lac"
+     
+     func removeFirstLast(_ string: String?) -> String {
+     }
+     */
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print(removeFirstLast("Hello"))
+        
     }
+    
+    func removeFirstLast(_ string: String?) -> String {
+        var internalString = Array(string!)
+        var index = 0
+        
+        while index != internalString.count {
+            if index == internalString.count - 1 {
+                print("1")
+                internalString.remove(at: index)
+                internalString.remove(at: 0)
+                break
+            } else {
+                index += 1
+                print("2")
+            }
+        }
+        return String(internalString)
+        
+    }
+    
 }
