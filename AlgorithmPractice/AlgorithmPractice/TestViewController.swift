7b16a71d58db5c706d25938cf1e98ab37ef19620
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-30 04:07:44 +0300

    Clean unused code

--- AlgorithmPractice/AlgorithmPractice/TestViewController.swift
+++ AlgorithmPractice/AlgorithmPractice/TestViewController.swift
@@ -10,24 +10,12 @@ import Foundation
 
 class TestViewController: UIViewController {
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(doubled2([1,2,3,4,5]))
-        print([2,3] + [1,3,2])
         
-    }
-    
-    func doubled2(_ array: Array<Int>) -> Array<Int> {
-        var internalArray: [Int] = []
-        
-        for element in array {
-            print(internalArray + [element * element])
-            print(internalArray + [1])
-        }
-        return internalArray
     }
     
     
