88738f30e52d64fe1a58e1d0ce114b8c4a2e0fb9
Arsenty Halimovsky <ghost12@yandex.ru>
2023-09-01 05:02:47 +0300

    Add solutior for problem N 33

--- AlgorithmPractice/AlgorithmPractice/TestViewController.swift
+++ AlgorithmPractice/AlgorithmPractice/TestViewController.swift
@@ -14,10 +14,37 @@ class TestViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        Int("5")
+        insertDash5(string: "454793")
     }
     
+    /*
+     33. Написать фукнцию которая проставит тире между нечетными числами
+     
+     "454793" -> "4547-9-3"
+     
+     func insertDash(string: String) -> String {
+     }
+     */
+    
+    func insertDash5(string: String) -> String {
+        var previousNumber = Int()
+        var secondNumber = Int()
+        var currentIndex = 0
+        var internalArray = Array(string)
+        
+        internalArray.map { value in
+            if Int(String(value))! % 2 != 0 &&
+                Int(String(internalArray[internalArray.firstIndex(of: value)! + 1]))! % 2 != 0 &&
+                currentIndex != internalArray.count - 2 {
+                print("Yo")
+                currentIndex += 1
+            }
+        }
+        
+        
+        return "hello"
+    }
 }
  
 
