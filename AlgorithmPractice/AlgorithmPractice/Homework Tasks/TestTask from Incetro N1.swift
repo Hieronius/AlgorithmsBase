311cb5147bcf3b761ffd937b4f92f204299b37fc
Arsenty Halimovsky <ghost12@yandex.ru>
2024-02-09 04:21:20 +0300

    Add test task N1 from incetro

---
+++ AlgorithmPractice/AlgorithmPractice/Homework Tasks/TestTask from Incetro N1.swift
@@ -0,0 +1,65 @@
+//
+//  main.swift
+//  3
+//
+//  Created by Арсентий Халимовский on 28.09.2023.
+//
+
+import Foundation
+
+// MARK: Workshop
+// https://incetro.notion.site/23e2860c1582430e9def233b594d8841
+
+func getPalindrome(_ word: String) -> Int {
+    var counter = 0
+    var halfOfWordToChange = word.count / 2
+
+    var immutableHalfOfWord = [String.Element]()
+    var mutableHalfOfWord = [String.Element]()
+    var arOfCharsToChange = [String.Element]()
+    
+    if word.count < 2 {
+        return 0
+        
+    } else if word.count == 2 {
+        return 1
+        
+    } else {
+        if word.count % 2 == 0 {
+            immutableHalfOfWord = Array(String(Array(word)[0..<halfOfWordToChange]))
+            mutableHalfOfWord = Array(String(Array(word)[halfOfWordToChange..<word.count]))
+            arOfCharsToChange = Array(String(immutableHalfOfWord.reversed()))
+            
+            for position in 1...halfOfWordToChange {
+                if arOfCharsToChange[position-1] == mutableHalfOfWord[position-1] {
+                    immutableHalfOfWord.append(arOfCharsToChange[position-1])
+                } else {
+                    immutableHalfOfWord.append(arOfCharsToChange[position-1])
+                    counter += 1
+                }
+                if String(immutableHalfOfWord) == String(word.reversed()) {
+                    break
+                }
+            }
+            
+        } else {
+            immutableHalfOfWord = Array(String(Array(word)[0...halfOfWordToChange]))
+            mutableHalfOfWord = Array(String(Array(word)[halfOfWordToChange..<word.count]))
+            arOfCharsToChange = Array(String(immutableHalfOfWord.reversed()))
+            
+            for position in 1...halfOfWordToChange {
+                if arOfCharsToChange[position] == mutableHalfOfWord[position] {
+                    immutableHalfOfWord.append(arOfCharsToChange[position])
+                } else {
+                    immutableHalfOfWord.append(arOfCharsToChange[position])
+                    counter += 1
+                }
+                if String(immutableHalfOfWord) == String(word.reversed()) {
+                    break
+                }
+            }
+        }
+    }
+    return counter
+}
+
